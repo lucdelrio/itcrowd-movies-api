29146de3ddbed642e027d917f32a161aaa15370c
Lucas del Rio <lucasgdelrio93@gmail.com>
2020-04-22 00:15:21 -0300

    Transform app to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :people
+  namespace :api do
+    namespace :v1 do
+      resources :movies
+      resources :people
+    end
+  end
 end
