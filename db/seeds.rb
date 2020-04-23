bf1eecb7d711acfc66260d6d5610ab37cbe8525c
Lucas del Rio <lucasgdelrio93@gmail.com>
2020-04-23 03:01:50 -0300

    Adds objects to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-
-Movie.create(title: 'Gladiator', release_year: 1999)
+Movie.create(title: 'Gladiator', release_year: 2000)
 Person.create(first_name: 'Russell', last_name: 'Crowe')
-Participation.create(person_id: Person.first.id, movie_id: Movie.first.id, role: 'actor')
+Participation.create(person: Person.first, movie: Movie.first, role: 'actor')
+
+Movie.create(title: 'Forest Gump', release_year: 1994)
+Person.create(first_name: 'Tom', last_name: 'Hanks')
+Person.create(first_name: 'Robert', last_name: 'Zemeckis')
+Person.create(first_name: 'Steve', last_name: 'Tisch')
+
+Participation.create(person_id: Person.find_by(last_name: 'Hanks').id, movie_id: Movie.find_by(title: 'Forest Gump').id, role: 'actor')
+Participation.create(person_id: Person.find_by(last_name: 'Zemeckis').id, movie_id: Movie.find_by(title: 'Forest Gump').id, role: 'director')
+Participation.create(person_id: Person.find_by(last_name: 'Tisch').id, movie_id: Movie.find_by(title: 'Forest Gump').id, role: 'producer')
 
 Admin.create(email: 'lucdelrio@itcrowd.com', password: 'itcrowd', password_confirmation: 'itcrowd')
