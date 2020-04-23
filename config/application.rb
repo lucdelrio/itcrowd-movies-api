29f202895636e0aeb9c69fcc3e4700d14f8869a6
Lucas del Rio <lucasgdelrio93@gmail.com>
2020-04-23 03:06:02 -0300

    Changes for Heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,8 @@ module ItCrowdMoviesApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.autoloader = :classic
+
     config.api_only = true
   end
 end
